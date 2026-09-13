0645356780b90ee97b7f7e4b62d8cebfc4f455e3
Austyn Morales <amorales14@buffs.wtamu.edu>
2026-09-13 18:33:40 -0500

    Add catalog models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+    class Book(models.Model):
+        title = models.CharField(max_length=200)
+        publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+        publication_year = models.IntegerField()
+
+        def __str__(self):
+            return self.title
+
+
+        class Review(models.Model):
+            body = models.TextField()
+            book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+            def __str__(self):
+                return self.body
